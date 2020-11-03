1f7ee80132835c876ec40dc58b5325b8c480b4a5
Дима <kolosow-dima@yandex.ru>
2020-11-03 17:37:56 +0500

    создал классы в Domain

---
+++ TaskMaster/Domain/ITask.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TaskMaster
+{
+    public interface ITask
+    {
+        string Message { get;}
+        TaskState State { get;}
+        DateTime Start { get;}
+        DateTime Finish { get;}
+    }
+}
---
+++ TaskMaster/Domain/TaskState.cs
@@ -0,0 +1,6 @@
+﻿namespace TaskMaster
+{
+    public enum TaskState
+    {
+    }
+}
